da20b9cc28914824aadbc1f96f6aedec741278eb
Myrosya-fsd <murosja80@gmail.com>
2025-04-04 19:29:35 +0300

    Initial commit

---
+++ src/js/2-form.js
@@ -0,0 +1,27 @@
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
+
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    formData.email = '';
+    formData.message = '';
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
+});
