f8b40fe70775c10c11e779c1f803ed6fbe9d58a0
myroslava <murosja80@gmail.com>
2025-04-05 16:08:05 +0300

    task_02

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,27 +1,8 @@
-const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
-  email: '',
-  message: '',
-};
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-const form = document.querySelector('.feedback-form');
-
-form.elements.email.value = formData.email;
-form.elements.message.value = formData.message;
-
-form.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value.trim();
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-});
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  if (!formData.email || !formData.message) {
-    alert('Fill please all fields');
-  } else {
-    console.log(formData);
-    formData.email = '';
-    formData.message = '';
-    localStorage.removeItem('feedback-form-state');
-    form.reset();
-  }
+const container = document.querySelector('.container-list');
+const accordion = new Accordion(container, {
+  showMultiple: true,
+  duration: 400,
 });
